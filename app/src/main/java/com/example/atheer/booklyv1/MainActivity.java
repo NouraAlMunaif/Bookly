ca4e2b33bd19f76f6d7122703170e5e4e780fe49
atheer <atheer@atheers-MacBook-Pro.local>
2018-09-18 17:28:53 +0300

    First commit

---
+++ app/src/main/java/com/example/atheer/booklyv1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.atheer.booklyv1;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+private static int logo_timeout = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+
+            }
+
+        },logo_timeout);
+    }
+}
